902126536fa7505a42ec05ff6d98ef844fd03df6
Shridhar Karigoudar <shree277004@gmail.com>
2022-10-25 15:10:08 +0530

    new db

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://admin:admin@10.1.1.5/sectest', {
 });
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error.:'));
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
 });
 
